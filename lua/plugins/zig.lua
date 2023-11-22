b84e77232cce0fde2064998ac4a3647d1535b0d9
Yuki Langley <hi@yukiisbo.red>
2023-11-22 13:27:24 +0700

    Add zig

---
+++ lua/plugins/zig.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "zig",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        zls = {
+          mason = false,
+        },
+      },
+    },
+  },
+}
