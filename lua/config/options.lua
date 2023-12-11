15597eca014a225b3026be9aba46b74e9cfd4752
Yuki Langley <hi@yukiisbo.red>
2023-12-11 13:36:57 +0700

    Change directory to %USERPROFILE% on Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local os = require("os")
+
 local opt = vim.opt
 
 opt.pumblend = 0
@@ -11,4 +13,14 @@ if vim.fn.has("win32") then
   opt.shellcmdflag = "-command"
   opt.shellquote = '\\"'
   opt.shellxquote = ""
+end
+
+if vim.fn.has("win32") then
+  local userprofile_path = os.getenv("USERPROFILE")
+  local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
+
+  vim.api.nvim_create_autocmd(
+    { "VimEnter" },
+    { pattern = "*", command = "cd " .. userprofile_path, group = vim_enter_group }
+  )
 end
